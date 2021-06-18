29e3a665c78061ea902d4f2716ee05f4c100e27d
Casper Schouls <casper.schouls@alliander.com>
2021-06-18 08:43:11 +0200

    Add process* functions which modify the req props

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,15 @@
-import { validateRequest, validateRequestBody, validateRequestParams, validateRequestQuery } from './index';
+import {
+  processRequest,
+  processRequestBody,
+  processRequestParams,
+  processRequestQuery,
+  validateRequest,
+  validateRequestBody,
+  validateRequestParams,
+  validateRequestQuery,
+} from './index';
 import { z } from 'zod';
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, request, Request, Response } from 'express';
 
 let nextFunction: NextFunction = jest.fn();
 
@@ -26,6 +35,53 @@ function mockResponse(): Partial<Response> {
   };
 }
 
+describe('Request body processor', () => {
+  beforeEach(() => {
+    sendMock.mockClear();
+  });
+  const bodySchema = z.object({ RequestBodyKey: z.string() });
+  it('Should call next() on succcesful validation', () => {
+    processRequestBody(bodySchema)(
+      mockRequest({ body: { RequestBodyKey: 'dit is de value' } }) as Request,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(nextFunction).toHaveBeenCalled();
+  });
+  it('Should send a HTTP400 on failed validation', () => {
+    const mockedResponse = mockResponse();
+    processRequestBody(z.object({ RequestBodyKey: z.string() }))(
+      mockRequest({ body: { RequestBodyKey: 12345 } }) as Request,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(mockedResponse.status).toHaveBeenCalledWith(400);
+  });
+  it('Should send a meaningful error on failed validation', () => {
+    const mockedResponse = mockResponse();
+    processRequestBody(z.object({ RequestBodyKey: z.string() }))(
+      mockRequest({ body: { RequestBodyKey: 12345 } }) as Request,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(sendMock).toHaveBeenCalledWith([{ type: 'Body', errors: expect.anything() }]);
+  });
+  it('Should modify the request body', () => {
+    const requestBody = {
+      RequestBodyKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      body: requestBody,
+    }) as Request;
+    processRequestBody(z.object({ RequestBodyKey: z.string() }))(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.body).toEqual({ RequestBodyKey: requestBody.RequestBodyKey });
+  });
+});
 describe('Request body validator', () => {
   beforeEach(() => {
     sendMock.mockClear();
@@ -56,6 +112,68 @@ describe('Request body validator', () => {
       nextFunction,
     );
     expect(sendMock).toHaveBeenCalledWith([{ type: 'Body', errors: expect.anything() }]);
+  });
+  it('Should not modify the request body', () => {
+    const requestBody = {
+      RequestBodyKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      body: requestBody,
+    }) as Request;
+    validateRequestBody(z.object({ RequestBodyKey: z.string() }))(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.body).toEqual(requestBody);
+  });
+});
+describe('Request query processor', () => {
+  beforeEach(() => {
+    sendMock.mockClear();
+  });
+  const querySchema = z.object({ RequestQueryKey: z.string() });
+  it('Should call next() on succcesful validation', () => {
+    processRequestQuery(querySchema)(
+      mockRequest({ query: { RequestQueryKey: 'dit is de value' } }) as any,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(nextFunction).toHaveBeenCalled();
+  });
+  it('Should send a HTTP400 on failed validation', () => {
+    const mockedResponse = mockResponse();
+    processRequestQuery(z.object({ RequestQueryKey: z.string() }))(
+      mockRequest({ query: { RequestQueryKey: 12345 } }) as any,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(mockedResponse.status).toHaveBeenCalledWith(400);
+  });
+  it('Should send a meaningful error on failed validation', () => {
+    const mockedResponse = mockResponse();
+    processRequestQuery(z.object({ RequestQueryKey: z.string() }))(
+      mockRequest({ query: { RequestQueryKey: 12345 } }) as any,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(sendMock).toHaveBeenCalledWith([{ type: 'Query', errors: expect.anything() }]);
+  });
+  it('Should modify the request query', () => {
+    const requestQuery = {
+      RequestQueryKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      query: requestQuery,
+    }) as any;
+    processRequestQuery(z.object({ RequestQueryKey: z.string() }))(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.query).toEqual({ RequestQueryKey: requestQuery.RequestQueryKey });
   });
 });
 describe('Request query validator', () => {
@@ -89,6 +207,68 @@ describe('Request query validator', () => {
     );
     expect(sendMock).toHaveBeenCalledWith([{ type: 'Query', errors: expect.anything() }]);
   });
+  it('Should not modify the request query', () => {
+    const requestQuery = {
+      RequestQueryKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      query: requestQuery,
+    }) as any;
+    validateRequestQuery(z.object({ RequestQueryKey: z.string() }))(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.query).toEqual(requestQuery);
+  });
+});
+describe('Request params processor', () => {
+  beforeEach(() => {
+    sendMock.mockClear();
+  });
+  const paramsSchema = z.object({ RequestParamsKey: z.string() });
+  it('Should call next() on succcesful validation', () => {
+    processRequestParams(paramsSchema)(
+      mockRequest({ params: { RequestParamsKey: 'dit is de value' } }) as any,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(nextFunction).toHaveBeenCalled();
+  });
+  it('Should send a HTTP400 on failed validation', () => {
+    const mockedResponse = mockResponse();
+    processRequestParams(z.object({ RequestParamsKey: z.string() }))(
+      mockRequest({ params: { RequestParamsKey: 12345 } }) as any,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(mockedResponse.status).toHaveBeenCalledWith(400);
+  });
+  it('Should send a meaningful error on failed validation', () => {
+    const mockedResponse = mockResponse();
+    processRequestParams(z.object({ RequestParamsKey: z.string() }))(
+      mockRequest({ params: { RequestParamsKey: 12345 } }) as any,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(sendMock).toHaveBeenCalledWith([{ type: 'Params', errors: expect.anything() }]);
+  });
+  it('Should modify the request params', () => {
+    const requestParams = {
+      RequestParamsKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      params: requestParams,
+    }) as any;
+    processRequestParams(z.object({ RequestParamsKey: z.string() }))(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.params).toEqual({ RequestParamsKey: requestParams.RequestParamsKey });
+  });
 });
 describe('Request params validator', () => {
   beforeEach(() => {
@@ -120,6 +300,205 @@ describe('Request params validator', () => {
       nextFunction,
     );
     expect(sendMock).toHaveBeenCalledWith([{ type: 'Params', errors: expect.anything() }]);
+  });
+  it('Should not modify the request params', () => {
+    const requestParams = {
+      RequestParamsKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      params: requestParams,
+    }) as any;
+    validateRequestParams(z.object({ RequestParamsKey: z.string() }))(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.params).toEqual(requestParams);
+  });
+});
+describe('Request processor', () => {
+  beforeEach(() => {
+    sendMock.mockClear();
+  });
+  const paramsSchema = z.object({ RequestParamsKey: z.string() });
+  const bodySchema = z.object({ RequestBodyKey: z.string() });
+  const querySchema = z.object({ RequestQueryKey: z.string() });
+  it('Should call next() on succcesful validation', () => {
+    processRequest({ body: bodySchema, params: paramsSchema, query: querySchema })(
+      mockRequest({
+        params: { RequestParamsKey: 'dit is de value' },
+        body: { RequestBodyKey: 'dit is de value' },
+        query: { RequestQueryKey: 'dit is de value' },
+      }) as any,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(nextFunction).toHaveBeenCalled();
+  });
+  it('Should send a HTTP400 on failed validation of params', () => {
+    const mockedResponse = mockResponse();
+    processRequest({ body: bodySchema, params: paramsSchema, query: querySchema })(
+      mockRequest({
+        params: { RequestParamsKey: 1234 },
+        body: { RequestBodyKey: 'dit is de value' },
+        query: { RequestQueryKey: 'dit is de value' },
+      }) as any,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(mockedResponse.status).toHaveBeenCalledWith(400);
+  });
+  it('Should send a HTTP400 on failed validation of body', () => {
+    const mockedResponse = mockResponse();
+    processRequest({ body: bodySchema, params: paramsSchema, query: querySchema })(
+      mockRequest({
+        params: { RequestParamsKey: 'dit is de value' },
+        body: { RequestBodyKey: 1234 },
+        query: { RequestQueryKey: 'dit is de value' },
+      }) as any,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(mockedResponse.status).toHaveBeenCalledWith(400);
+  });
+  it('Should send a HTTP400 on failed validation of query', () => {
+    const mockedResponse = mockResponse();
+    processRequest({ body: bodySchema, params: paramsSchema, query: querySchema })(
+      mockRequest({
+        params: { RequestParamsKey: 'dit is de value' },
+        body: { RequestBodyKey: 'dit is de value' },
+        query: { RequestQueryKey: 1234 },
+      }) as any,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(mockedResponse.status).toHaveBeenCalledWith(400);
+  });
+  it('Should send a meaningful error on failed validation of params', () => {
+    const mockedResponse = mockResponse();
+    processRequest({ body: bodySchema, params: paramsSchema, query: querySchema })(
+      mockRequest({
+        params: { RequestParamsKey: 1234 },
+        body: { RequestBodyKey: 'dit is de value' },
+        query: { RequestQueryKey: 'dit is de value' },
+      }) as any,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(sendMock).toHaveBeenCalledWith([{ type: 'Params', errors: expect.anything() }]);
+  });
+  it('Should send a meaningful error on failed validation of body', () => {
+    const mockedResponse = mockResponse();
+    processRequest({ body: bodySchema, params: paramsSchema, query: querySchema })(
+      mockRequest({
+        params: { RequestParamsKey: 'dit is de value' },
+        body: { RequestBodyKey: 1234 },
+        query: { RequestQueryKey: 'dit is de value' },
+      }) as any,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(sendMock).toHaveBeenCalledWith([{ type: 'Body', errors: expect.anything() }]);
+  });
+  it('Should send a meaningful error on failed validation of query', () => {
+    const mockedResponse = mockResponse();
+    processRequest({ body: bodySchema, params: paramsSchema, query: querySchema })(
+      mockRequest({
+        params: { RequestParamsKey: 'dit is de value' },
+        body: { RequestBodyKey: 'dit is de value' },
+        query: { RequestQueryKey: 1234 },
+      }) as any,
+      mockedResponse as Response,
+      nextFunction,
+    );
+    expect(sendMock).toHaveBeenCalledWith([{ type: 'Query', errors: expect.anything() }]);
+  });
+  it('Should modify the request params', () => {
+    const requestParams = {
+      RequestParamsKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      params: requestParams,
+    }) as any;
+    processRequest({ params: z.object({ RequestParamsKey: z.string() }) })(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.params).toEqual({
+      RequestParamsKey: requestParams.RequestParamsKey,
+    });
+  });
+  it('Should modify the request query', () => {
+    const requestQuery = {
+      RequestQueryKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      query: requestQuery,
+    }) as any;
+    processRequest({ query: z.object({ RequestQueryKey: z.string() }) })(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.query).toEqual({
+      RequestQueryKey: requestQuery.RequestQueryKey,
+    });
+  });
+
+  it('Should modify the request body', () => {
+    const requestBody = {
+      RequestBodyKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      body: requestBody,
+    }) as Request;
+    processRequest({ body: z.object({ RequestBodyKey: z.string() }) })(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.body).toEqual({
+      RequestBodyKey: requestBody.RequestBodyKey,
+    });
+  });
+
+  it('Should modify the request', () => {
+    const requestBody = {
+      RequestBodyKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const requestQuery = {
+      RequestQueryKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const requestParams = {
+      RequestParamsKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      params: requestParams,
+      body: requestBody,
+      query: requestQuery,
+    }) as any;
+    processRequest({
+      body: z.object({ RequestBodyKey: z.string() }),
+      query: z.object({ RequestQueryKey: z.string() }),
+    })(mockedRequest, mockResponse() as Response, nextFunction);
+    expect(mockedRequest.body).toEqual({
+      RequestBodyKey: requestBody.RequestBodyKey,
+    });
+    expect(mockedRequest.query).toEqual({
+      RequestQueryKey: requestQuery.RequestQueryKey,
+    });
+    expect(mockedRequest.params).toEqual({
+      RequestParamsKey: requestParams.RequestParamsKey,
+      SomeOtherRandomValue: requestParams.SomeOtherRandomValue,
+    });
   });
 });
 describe('Request validator', () => {
@@ -218,5 +597,78 @@ describe('Request validator', () => {
       nextFunction,
     );
     expect(sendMock).toHaveBeenCalledWith([{ type: 'Query', errors: expect.anything() }]);
+  });
+  it('Should not modify the request params', () => {
+    const requestParams = {
+      RequestParamsKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      params: requestParams,
+    }) as any;
+    validateRequest({ params: z.object({ RequestParamsKey: z.string() }) })(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.params).toEqual(requestParams);
+  });
+  it('Should not modify the request query', () => {
+    const requestQuery = {
+      RequestQueryKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      query: requestQuery,
+    }) as any;
+    validateRequest({ query: z.object({ RequestQueryKey: z.string() }) })(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.query).toEqual(requestQuery);
+  });
+
+  it('Should not modify the request body', () => {
+    const requestBody = {
+      RequestBodyKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      body: requestBody,
+    }) as Request;
+    validateRequest({ body: z.object({ RequestBodyKey: z.string() }) })(
+      mockedRequest,
+      mockResponse() as Response,
+      nextFunction,
+    );
+    expect(mockedRequest.body).toEqual(requestBody);
+  });
+
+  it('Should not modify the request', () => {
+    const requestBody = {
+      RequestBodyKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const requestQuery = {
+      RequestQueryKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const requestParams = {
+      RequestParamsKey: "This is the one we're parsing",
+      SomeOtherRandomValue: 'This one should not be removed',
+    };
+    const mockedRequest = mockRequest({
+      params: requestParams,
+      body: requestBody,
+      query: requestQuery,
+    }) as any;
+    validateRequest({
+      body: z.object({ RequestBodyKey: z.string() }),
+      query: z.object({ RequestQueryKey: z.string() }),
+    })(mockedRequest, mockResponse() as Response, nextFunction);
+    expect(mockedRequest.body).toEqual(requestBody);
+    expect(mockedRequest.query).toEqual(requestQuery);
+    expect(mockedRequest.params).toEqual(requestParams);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { RequestHandler, Response } from 'express';
 import { ParamsDictionary } from 'express-serve-static-core';
-import { ZodError, ZodSchema } from 'zod';
+import { ZodEffects, ZodError, ZodSchema } from 'zod';
 
 type NonReadOnly<T> = { -readonly [P in keyof T]: NonReadOnly<T[P]> };
 
@@ -16,6 +16,122 @@ export const sendErrors: (errors: Array<ErrorListItem>, res: Response) => void =
 export const sendError: (error: ErrorListItem, res: Response) => void = (error, res) => {
   return res.status(400).send({ type: error.type, errors: error.errors });
 };
+
+export function processRequestBody<TBody>(effects: ZodSchema<TBody>): RequestHandler<ParamsDictionary, any, TBody, any>;
+export function processRequestBody<TBody>(
+  effects: ZodEffects<any, TBody>,
+): RequestHandler<ParamsDictionary, any, TBody, any>;
+export function processRequestBody<TBody>(
+  effectsSchema: ZodEffects<any, TBody> | ZodSchema<TBody>,
+): RequestHandler<ParamsDictionary, any, TBody, any> {
+  return (req, res, next) => {
+    const parsed = effectsSchema.safeParse(req.body);
+    if (parsed.success) {
+      req.body = parsed.data;
+      return next();
+    } else {
+      return sendErrors([{ type: 'Body', errors: parsed.error }], res);
+    }
+  };
+}
+
+export function processRequestParams<TParams>(effects: ZodSchema<TParams>): RequestHandler<TParams, any, any, any>;
+export function processRequestParams<TParams>(
+  effects: ZodEffects<any, TParams>,
+): RequestHandler<TParams, any, any, any>;
+export function processRequestParams<TParams>(
+  effectsSchema: ZodEffects<any, TParams> | ZodSchema<TParams>,
+): RequestHandler<TParams, any, any, any> {
+  return (req, res, next) => {
+    const parsed = effectsSchema.safeParse(req.params);
+    if (parsed.success) {
+      req.params = parsed.data;
+      return next();
+    } else {
+      return sendErrors([{ type: 'Params', errors: parsed.error }], res);
+    }
+  };
+}
+
+export function processRequestQuery<TQuery>(
+  effects: ZodSchema<TQuery>,
+): RequestHandler<ParamsDictionary, any, any, TQuery>;
+export function processRequestQuery<TQuery>(
+  effects: ZodEffects<any, TQuery>,
+): RequestHandler<ParamsDictionary, any, any, TQuery>;
+export function processRequestQuery<TQuery>(
+  effectsSchema: ZodEffects<any, TQuery> | ZodSchema<TQuery>,
+): RequestHandler<ParamsDictionary, any, any, TQuery> {
+  return (req, res, next) => {
+    const parsed = effectsSchema.safeParse(req.query);
+    if (parsed.success) {
+      req.query = parsed.data;
+      return next();
+    } else {
+      return sendErrors([{ type: 'Query', errors: parsed.error }], res);
+    }
+  };
+}
+
+export function processRequest<TParams = any, TQuery = any, TBody = any>(
+  schemas: Partial<{
+    params: ZodEffects<any, TParams>;
+    query: ZodEffects<any, TQuery>;
+    body: ZodEffects<any, TBody>;
+  }>,
+): RequestHandler<TParams, any, TBody, TQuery>;
+export function processRequest<TParams = any, TQuery = any, TBody = any>(
+  schemas: Partial<{
+    params: ZodSchema<TParams>;
+    query: ZodSchema<TQuery>;
+    body: ZodSchema<TBody>;
+  }>,
+): RequestHandler<TParams, any, TBody, TQuery>;
+export function processRequest<TParams = any, TQuery = any, TBody = any>(
+  schemas:
+    | Partial<{
+        params: ZodSchema<TParams>;
+        query: ZodSchema<TQuery>;
+        body: ZodSchema<TBody>;
+      }>
+    | Partial<{
+        params: ZodEffects<any, TParams>;
+        query: ZodEffects<any, TQuery>;
+        body: ZodEffects<any, TBody>;
+      }>,
+): RequestHandler<TParams, any, TBody, TQuery> {
+  return (req, res, next) => {
+    const errors: Array<ErrorListItem> = [];
+    if (schemas.params) {
+      const parsed = schemas.params.safeParse(req.params);
+      if (parsed.success) {
+        req.params = parsed.data;
+      } else {
+        errors.push({ type: 'Params', errors: parsed.error });
+      }
+    }
+    if (schemas.query) {
+      const parsed = schemas.query.safeParse(req.query);
+      if (parsed.success) {
+        req.query = parsed.data;
+      } else {
+        errors.push({ type: 'Query', errors: parsed.error });
+      }
+    }
+    if (schemas.body) {
+      const parsed = schemas.body.safeParse(req.body);
+      if (parsed.success) {
+        req.body = parsed.data;
+      } else {
+        errors.push({ type: 'Body', errors: parsed.error });
+      }
+    }
+    if (errors.length > 0) {
+      return sendErrors(errors, res);
+    }
+    return next();
+  };
+}
 
 export const validateRequestBody: <TBody>(
   zodSchema: ZodSchema<TBody>,
